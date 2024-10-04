e8d1fa05637cd2027c38d35ee7015d91af33ac03
charlyb01 <charly5bollinger@gmail.com>
2024-10-04 14:28:10 +0200

    Init mod project

---
+++ src/client/java/com/github/charlyb01/sihywtcamd_extensions/SihywtcamdExtensionsClient.java
@@ -0,0 +1,10 @@
+package com.github.charlyb01.sihywtcamd_extensions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SihywtcamdExtensionsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/github/charlyb01/sihywtcamd_extensions/SihywtcamdExtensions.java
@@ -0,0 +1,10 @@
+package com.github.charlyb01.sihywtcamd_extensions;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SihywtcamdExtensions implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
